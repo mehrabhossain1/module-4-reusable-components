2aee16dc0fe0233fbc27f76dbf8a75dcc75e604d
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-01-16 19:23:08 +0600

    modal

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -4,11 +4,14 @@ import { createPortal } from 'react-dom'
 const Modal = ({ isOpen, onClose, children }) => {
   return createPortal(
     <div
-      className={cn('fixed inset-0 bg-gray-500/70 invisible z-[999]', {
-        visible: isOpen,
-      })}
+      className={cn(
+        'fixed inset-0 flex justify-center items-center bg-gray-500/70 invisible z-[999]',
+        {
+          visible: isOpen,
+        }
+      )}
     >
-      <div className='bg-white'>{children}</div>
+      <div className='bg-white w-full max-w-sm rounded-md'>{children}</div>
     </div>,
     document.getElementById('portal') as Element
   )
