9d7c24b228a7d818697ce1880d86e0a4f253b986
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-01-16 20:49:23 +0600

    4.10

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
-import { useState } from 'react'
-import Button from './components/ui/Button'
-import Container from './components/ui/Container'
-import Modal from './components/ui/Modal'
+import { useState } from "react";
+import Button from "./components/ui/Button";
+import Container from "./components/ui/Container";
+import Modal from "./components/ui/Modal";
 
 function App() {
-  const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false);
 
   const handleModalClose = () => {
-    setModal((prev) => !prev)
-  }
+    setModal((prev) => !prev);
+  };
 
   return (
     <Container>
-      <div className='relative h-screen w-full flex justify-center items-center'>
-        <Button className='z-10' onClick={() => setModal((prev) => !prev)}>
+      <div className="relative h-screen w-full flex justify-center items-center">
+        <Button
+          className="z-10"
+          onClick={() => setModal((prev) => !prev)}
+        >
           Open Modal
         </Button>
-        <Modal isOpen={modal} onClose={handleModalClose}>
-          <Modal.CloseButton />
-          <h1>This is a Modal</h1>
+        <Modal
+          isOpen={modal}
+          onClose={handleModalClose}
+        >
+          <Modal.Header>
+            <h1>This is modal title</h1>
+            <Modal.CloseButton></Modal.CloseButton>
+          </Modal.Header>
+          <p>This is a Modal content</p>
         </Modal>
       </div>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
